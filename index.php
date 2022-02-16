d5b804119454ff29cfab7facb0d8feb1544c45de
Garasi Pelosok <garasipelosok@gmail.com>
2022-02-16 09:12:41 +0700

    add paragraph to index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hallo Student!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quos illum eos exercitationem repellendus voluptatem ducimus voluptas. Quod eligendi amet ducimus unde, tenetur ipsa, veritatis repellendus soluta commodi, qui nulla?</p>
 </body>
 </html>
