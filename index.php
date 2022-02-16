4caec931567f63294a71dc6734191537daf2614e
Garasi Pelosok <garasipelosok@gmail.com>
2022-02-16 09:50:49 +0700

    add list to index.php

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hallo Student!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quos illum eos exercitationem repellendus voluptatem ducimus voluptas. Quod eligendi amet ducimus unde, tenetur ipsa, veritatis repellendus soluta commodi, qui nulla?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
